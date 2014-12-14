f6e5f30f38c59c338e93d1510fb68cd630520c7a
DrKondor <snakey@deltadevils.net>
2014-12-13 23:10:20 -0700

    Update Caps2Super.sh

--- Caps2Super.sh
+++ Caps2Super.sh
@@ -1,3 +1,3 @@
-#This little thing is for my Cherry g84 keyboard, which does not have a windows or "Super" key. So that useless hunk of plastic labelled Caps Lock can stand in. This should work with any x11 based system. 
+#This little thing is for my Cherry g84 keyboard, which does not have a windows or "Super" key. So, that useless hunk of plastic labelled Caps Lock can stand in. This should work with any x11 based system. 
 
 setxkbmap -option caps:super
